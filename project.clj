7588dddeaf7e4bd2ad143a16ff568702b0762a83
Julio Berina <julioberina@gmail.com>
2018-01-13 16:46:39 -0800

    Installed Selmer and rendered a couple of files

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
+                 [selmer "1.11.5"]
                  [ring/ring-defaults "0.2.1"]]
   :plugins [[lein-ring "0.9.7"]]
+  :resource-paths ["resources/public"]
   :ring {:handler chitchat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/chitchat/handler.clj
+++ src/chitchat/handler.clj
@@ -1,10 +1,12 @@
 (ns chitchat.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [selmer.parser :refer :all]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (render-file "index.html" {}))
+  (GET "/about" [] (render-file "about.html" {}))
   (route/not-found "Not Found"))
 
 (def app
